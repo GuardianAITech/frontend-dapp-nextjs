7b04b85e3a8ae2bdcf16d7264975b85aa0b99cab
Belijal <119926580+Belijal@users.noreply.github.com>
2024-01-13 21:29:26 +0100

    Prepared Approval page and added Overview Card

--- app/approval/page.js
+++ app/approval/page.js
@@ -1,12 +1,16 @@
 import React from 'react'
-
+import ApprovalOverview from '@/components/dashboard/Approval';
 
 const page = async () => {
     
     return (
         <div>
-            
-
+            <div className="flex flex-col gap-5 flex-[3] md:w-4/4 xl:w-5/5">
+                <div className='flex justify-start items-center mb-5 gap-10'>
+                    <h1 className="text-5xl font-light regular-text text-white">Approval Check</h1>
+                </div>
+                <ApprovalOverview/>
+            </div>
         </div>
     )
 }
---
+++ components/dashboard/Approval.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ApprovalOverview = () => {
+    return (
+        <div className='h-[450px] p-5 rounded-lg softBg'>
+            <h2 className='mb-5 font-light regular-text text-2xl'>Approval Overview</h2>
+                <span className="text-sm text-gray-200">Review all your approvals and instantly revoke them if neccessary!</span>
+            
+        </div>
+    );
+
+}
+
+export default ApprovalOverview
