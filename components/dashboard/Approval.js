5ea1b69a9877f2afebf799009d225b127dade97e
Belijal <119926580+Belijal@users.noreply.github.com>
2024-01-18 21:46:21 +0100

    Added Loading Modal + Placeholder image

--- components/dashboard/Approval.js
+++ components/dashboard/Approval.js
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from 'react';
 import ABI from '../../ABI/erc20abi.json'
 import { useWeb3ModalAccount, useWeb3ModalProvider } from '@web3modal/ethers/react';
 import { BrowserProvider, Contract } from 'ethers'
+import { MdSearch } from "react-icons/md";
+
 
 const SuccessModal = ({ show, onClose }) => {
     if (!show) return null;
@@ -27,6 +29,23 @@ const SuccessModal = ({ show, onClose }) => {
     );
 };
 
+const LoadingModal = ({ show }) => {
+    if (!show) return null;
+  
+    return (
+        <div className="fixed inset-0 bg-black bg-opacity-70 flex items-center justify-center z-50">
+            <div className="bg-[#1E1E2E] rounded-lg p-6 w-full max-w-md mx-auto text-center">
+                <div className="flex justify-center">
+                    <div className="border-gray-300 h-20 w-20 animate-spin rounded-full border-8 border-t-blue-600" />
+                </div>
+                <p className="mt-2 text-lg text-white">
+                    Loading Approval Data...
+                </p>
+            </div>
+        </div>
+    );
+  }
+
 const ApprovalOverview = () => {
 
     const { isConnected, address } = useWeb3ModalAccount();
@@ -34,11 +53,20 @@ const ApprovalOverview = () => {
     const [approvals, setApprovals] = useState(null);
     const { walletProvider } = useWeb3ModalProvider();
     const [showSuccessModal, setShowSuccessModal] = useState(false);
+    const [inputaddress, setInputaddress] = useState('');
+    const [manualscan, setManualscan] = useState(false);
 
-    const fetchApprovals = async () => {
-        if (isConnected && address) {
+    const onStartScanClick = () => {
+        if (inputaddress) {
+          fetchApprovals(inputaddress)
+          setManualscan(true)
+        }
+    };
+
+    const fetchApprovals = async (providedaddress) => {
+        setIsDataReady(false);
             try {
-                const response = await fetch(`/api/approval?wallet=${address}`);
+                const response = await fetch(`/api/approval?wallet=${providedaddress}`);
                 if (!response.ok) {
                     throw new Error(`HTTP error! status: ${response.status}`);
                 }
@@ -49,11 +77,12 @@ const ApprovalOverview = () => {
             } catch (error) {
                 console.error('API call error:', error);
             }
-        }
     };
 
     useEffect(() => {
-        fetchApprovals();
+        if (isConnected && address) {
+          fetchApprovals(address);
+        }
     }, [isConnected, address]);
 
     async function revoke(tokenAddress, spenderAddress){
@@ -77,16 +106,67 @@ const ApprovalOverview = () => {
 
     const closeModal = () => {
         setShowSuccessModal(false);
-        fetchApprovals();
+        fetchApprovals(address);
     };
+
+    if (!isConnected && !manualscan) {
+        return (
+          <div className="flex flex-col gap-5 flex-[3] md:w-4/4 xl:w-5/5">
+            <div className='flex flex-col md:flex-row justify-between items-center mb-5 gap-4 md:gap-10'>
+              <h1 className="text-3xl md:text-5xl font-light regular-text text-white">Approval Check</h1>
+              <div className="flex items-center gap-4 md:gap-12">
+                <img src="/images/logo.webp" alt="Guardian AI Logo" className="h-16 md:h-20" />
+                <w3m-button className="text-[#00D2FF]"></w3m-button>
+              </div>
+            </div>
+            <div className="flex flex-col items-center gap-4 md:flex-row md:justify-start md:items-center">
+              <div className="flex items-center gap-4">
+                <div className="flex items-center gap-2 bg-[#20202c] p-2 rounded-lg">
+                  <MdSearch size={20} className="text-[#8692A6]" />
+                  <input
+                    type="text"
+                    placeholder="Scan by Address"
+                    className="bg-transparent border-none text-[#8692A6] placeholder-[#8692A6]"
+                    value={inputaddress}
+                    onChange={(e) => setInputaddress(e.target.value)}
+                  />
+                </div>
+                <button onClick={onStartScanClick} className="bg-[#00D2FF] text-white px-4 py-2 rounded-lg">
+                  Start Scan
+                </button>
+              </div>
+            </div>
+            <div className='h-[450px] softBg p-5 rounded-lg flex justify-center items-center'>
+              <h2 className='mb-5 font-light regular-text text-center items-center text-2xl text-white'>Please Connect your wallet or scan manually!</h2>
+            </div>
+          </div>
+        );
+    }
 
     return (
         <div className="flex flex-col gap-5 flex-[3] md:w-4/4 xl:w-5/5">
             <div className='flex flex-col md:flex-row justify-between items-center mb-5 gap-4 md:gap-10'>
-                <h1 className="text-3xl md:text-5xl font-light regular-text text-white">Quick Scan</h1>
+                <h1 className="text-3xl md:text-5xl font-light regular-text text-white">Approval Check</h1>
                 <div className="flex items-center gap-4 md:gap-12">
                     <img src="/images/logo.webp" alt="Guardian AI Logo" className="h-16 md:h-20" />
                     <w3m-button className="text-[#00D2FF]"></w3m-button>
+                </div>
+            </div>
+            <div className="flex flex-col items-center gap-4 md:flex-row md:justify-start md:items-center">
+                <div className="flex items-center gap-4">
+                    <div className="flex items-center gap-2 bg-[#20202c] p-2 rounded-lg">
+                        <MdSearch size={20} className="text-[#8692A6]" />
+                        <input
+                            type="text"
+                            placeholder="Scan by Address"
+                            className="bg-transparent border-none text-[#8692A6] placeholder-[#8692A6]"
+                            value={inputaddress}
+                            onChange={(e) => setInputaddress(e.target.value)}
+                        />
+                    </div>
+                    <button onClick={onStartScanClick} className="bg-[#00D2FF] text-white px-4 py-2 rounded-lg">
+                        Start Scan
+                    </button>
                 </div>
             </div>
             <div className='p-5 rounded-lg softBg'>
@@ -116,12 +196,31 @@ const ApprovalOverview = () => {
                                         style={{ backgroundColor: '#1E1E2E' }}
                                     >
                                         <div className="col-span-1 flex items-center justify-center">
-                                            <img src={item.logo_url} alt={`${item.ticker_symbol} Logo`} className="w-4 h-4 mr-2" />
+                                            {item.logo_url ? (
+                                                <img
+                                                    src={item.logo_url || '/images/noimg.png'}
+                                                    alt={`${item.ticker_symbol} Logo`}
+                                                    className="w-4 h-4 mr-2"
+                                                    onError={(e) => {
+                                                        e.preventDefault();
+                                                    e.target.src = '/images/noimg.png'; 
+                                                    }}
+                                                />
+                                            ) : (
+                                                <div className="w-4 h-4 bg-gray-300 rounded-full mr-2"></div>
+                                            )}
                                             {item.ticker_symbol}
                                         </div>
-                                        <div className="col-span-2 flex items-center justify-center">{item.pretty_balance_quote}</div>
-                                        <div className="col-span-2 flex items-center justify-center">{item.pretty_value_at_risk_quote}</div>
-                                        <div className="col-span-2 flex items-center justify-center">{spender.pretty_allowance_quote}</div>
+                                        <div className="col-span-2 flex items-center justify-center">
+                                            {item.pretty_balance_quote ?? '0'}
+                                        </div>
+                                        <div className="col-span-2 flex items-center justify-center">
+                                            {item.pretty_value_at_risk_quote ?? '0'}
+                                        </div>
+                                        <div className="col-span-2 flex items-center justify-center">
+                                            {spender.pretty_allowance_quote ?? spender.allowance}
+                                        </div>
+
                                         <a
                                             href={`https://etherscan.io/address/${spender.spender_address}`}
                                             target="_blank"
@@ -130,12 +229,17 @@ const ApprovalOverview = () => {
                                             title={spender.spender_address}>
                                             {spender.spender_address}
                                         </a>
-                                        <div className="col-span-1 flex items-center justify-center">{spender.risk_factor}</div>
+                                        <div className="col-span-1 flex items-center justify-center">{spender.risk_factor ?? 'NO RISK'}</div>
                                         <button
-                                            className="col-span-2 bg-[#00D2FF] hover:bg-[#009cbf] text-white font-bold py-1 px-4 rounded"
-                                            onClick={() => revoke(item.token_address, spender.spender_address )}
+                                            className={`bg-[#00D2FF] hover:bg-[#009cbf] text-white font-bold py-1 px-4 rounded w-full ${manualscan ? 'opacity-50 cursor-not-allowed' : ''}`}
+                                            onClick={() => {
+                                                if (!manualscan) {
+                                                    revoke(item.token_address, spender.spender_address);
+                                                }
+                                            }}
+                                            disabled={manualscan}
                                         >
-                                        REVOKE
+                                            REVOKE
                                         </button>
                                     </div>
                                 ))
@@ -152,17 +256,22 @@ const ApprovalOverview = () => {
                                     >
                                         <div className="text-sm text-white">
                                             <div className="mb-1"><span className="font-bold">Asset:</span> {item.ticker_symbol}</div>
-                                            <div className="mb-1"><span className="font-bold">Balance:</span> {item.pretty_balance_quote}</div>
-                                            <div className="mb-1"><span className="font-bold">Value at Risk:</span> {item.pretty_value_at_risk_quote}</div>
-                                            <div className="mb-1"><span className="font-bold">Allowance:</span> {spender.pretty_allowance_quote}</div>
+                                            <div className="mb-1"><span className="font-bold">Balance:</span> {item.pretty_balance_quote ?? '0'}</div>
+                                            <div className="mb-1"><span className="font-bold">Value at Risk:</span> {item.pretty_value_at_risk_quote ?? '0'}</div>
+                                            <div className="mb-1"><span className="font-bold">Allowance:</span> {spender.pretty_allowance_quote ?? spender.allowance}</div>
                                             <div className="mb-1"><span className="font-bold">Spender:</span> {spender.spender_address}</div>
-                                            <div className="mb-1"><span className="font-bold">Risk Factor:</span> {spender.risk_factor}</div>
+                                            <div className="mb-1"><span className="font-bold">Risk Factor:</span> {spender.risk_factor ?? 'NO RISK'}</div>
                                         </div>
                                         <button
-                                            className="bg-[#00D2FF] hover:bg-[#009cbf] text-white font-bold py-1 px-4 rounded w-full"
-                                            onClick={() => revoke(item.token_address, spender.spender_address )}
+                                            className={`bg-[#00D2FF] hover:bg-[#009cbf] text-white font-bold py-1 px-4 rounded w-full ${manualscan ? 'opacity-50 cursor-not-allowed' : ''}`}
+                                            onClick={() => {
+                                                if (!manualscan) {
+                                                    revoke(item.token_address, spender.spender_address);
+                                                }
+                                            }}
+                                            disabled={manualscan}
                                         >
-                                        REVOKE
+                                            REVOKE
                                         </button>
                                     </div>
                                 ))
@@ -173,6 +282,7 @@ const ApprovalOverview = () => {
                 {showSuccessModal && (
                     <SuccessModal show={showSuccessModal} onClose={closeModal} />
                 )}
+                <LoadingModal show={!isDataReady} />
             </div>
         </div>
     ); 
